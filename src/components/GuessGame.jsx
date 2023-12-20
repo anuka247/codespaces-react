01ec5cc808eb42b408d158ddf51c9c127818cfc4
anuka247 <148517097+anuka247@users.noreply.github.com>
2023-12-20 01:18:23 +0000

    update game

--- src/components/GuessGame.jsx
+++ src/components/GuessGame.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import './style.css'
+import one from "../components/img/onePiece..webp"
 const GuessEmojiGame = () => {
     const [hariult, setHariult] = useState('')
     const [result, setResult] = useState('')
     const asuult = {
         zovHariu: 'One piece',
+        zurag: one , 
         emoji: "🏴‍☠️👒🍖🐒⛵️",
         songoltuud: ['One piece', 'Naruto', 'Demon slayer', 'Attack on titan']
     }
@@ -16,11 +18,14 @@ const GuessEmojiGame = () => {
     return (
         <div className='guess'>
             <div className='main'>
-                <h1 className='title'>Guess the emoji: {result} </h1>
-                <h2 className='emoji'> {asuult.emoji} </h2>
+                <h1 className='title'>Guess the emoji game  {result} </h1>
+                <h2 className='emoji'> { hariult ? <img src={asuult.zurag} width={350} height={180}/> : asuult.emoji} </h2>
                 <div className='answers'>
                     {asuult.songoltuud.map((songolt, i) =>
-                            <button className={hariult === songolt && (asuult.zovHariu === hariult ? 'nogoon' : 'ulaan ') }onClick={() => handleClick(songolt)}>{songolt}</button>)}
+                    
+                            <button className={hariult === songolt && (asuult.zovHariu === hariult ? 'nogoon' : 'ulaan ') }
+                            disabled={hariult}
+                            onClick={() => handleClick(songolt)}>{songolt}</button>)}
                 </div>
             </div>
         </div>
